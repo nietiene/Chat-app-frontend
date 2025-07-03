effd67b8b2a861622b03ea7bce0a9eabd8898160
nietiene <niyomugaboetiene53@gmail.com>
2025-07-03 14:15:44 -0700

    first commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,4 +6,15 @@ import { useState } from "react";
 export default function Login() {
     const [phone, setPhone] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const res = await api.post('/api/auth/login', { phone, password });
+            alert(res.data.message);
+            navigate('/dashboard');
+        }
+    }
 }
