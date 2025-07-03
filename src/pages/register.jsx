cf7a02efb61f397de0d6ac651b75e1ca048a0b0a
nietiene <niyomugaboetiene53@gmail.com>
2025-07-03 14:41:06 -0700

    first commit

---
+++ src/pages/register.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api";
+
+
+export default function Register() {
+    const [from, setForm] = useState({name: '', phone: '', password: ''});
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+      setForm({...from, [e.target.name] : [e.target.value] });
+    }
+
+    const handleSubmit = async (e) {
+        
+    }
+}
