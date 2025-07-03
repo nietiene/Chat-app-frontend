260bd27856d3c1a0f03875b10ab003bb2883f59b
nietiene <niyomugaboetiene53@gmail.com>
2025-07-03 14:07:26 -0700

    first commit

---
+++ src/pages/login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import api from "..api.js";
+import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+
+export default function Login() {
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+}
