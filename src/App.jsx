c019b09cdabd830a95c7417f2a440704030721d2
nietiene <niyomugaboetiene53@gmail.com>
2025-07-03 16:10:11 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Login from "./pages/login"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Register from "./pages/register"
 import Dashboard from "./pages/dashboard"
+import Chat from "./pages/chat"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/chat" element={<Chat />}/>
          </Routes>
 
   )
