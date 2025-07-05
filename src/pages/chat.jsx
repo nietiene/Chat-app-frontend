7a845a755055cc05c7f2fad5aa7753c1fe30d354
nietiene <niyomugaboetiene53@gmail.com>
2025-07-05 10:00:51 -0700

    commit of gikonko-tss-chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -107,7 +107,8 @@ export default function Chat() {
                         >
                             {/* Avatar */}
                             <div className="w-10 h-10 rounded-full bg-blue-500 text-white flex items-center justify-center text-lg">
-                                
+                                {user.charAt(0).toUpperCase()}
+        
                             </div>
                         </button>
                 </li>
