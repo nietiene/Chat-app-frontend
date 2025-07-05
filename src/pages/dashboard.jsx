d2ef6a9334d6b35bb76d15b5223692400581807a
nietiene <niyomugaboetiene53@gmail.com>
2025-07-05 08:42:38 -0700

    commit of gikonko-tss-chat

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,7 +2,6 @@ import api from "../api";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
-import  { FaHome, FaEnvelope, FaBell, FaUserCircle } from "react-icons/fa";
 
 export default function Dashboard() {
     const [user, setUser] = useState("");
@@ -25,25 +24,7 @@ export default function Dashboard() {
     if (!user) return <p className="text-center mt-10">Loading.....</p>
 
     return (
-        <div className="min-h-screen flex flex-col">
-               <nav className="flex justify-between items-center bg-blue-700 text-white px-6 py-3 shadow">
-                <div className="flex gap-6 items-center">
-                    <Link className="flex items-center gap-1 hover:underline">
-                         <FaHome/> Home
-                    </Link>
-                    <Link className="flex items-center gap-1 hover:underline">
-                          <FaEnvelope/> Messages
-                    </Link>
-                    <Link className="flex items-center gap-1 hover:underline">
-                        <FaBell/> Notification
-                    </Link>
-                </div>
-                
-                <button className="bg-white text-blue-700 px-3 py-2 rounded-xl font-semibold flex flex-col items-center">
-                    <FaUserCircle className="text-3xl mb-1"/>
-                </button>
-               </nav>
-
+      <div>
 {/* Left side profile */}
           <div className="flex flex-1">
              <aside className="w-64 bg-gray-100 p-4 border-r shadowsm">
---
+++ src/pages/layout.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Outlet,Link } from "react-router-dom";
+import  { FaHome, FaEnvelope, FaBell, FaUserCircle } from "react-icons/fa";
+
+
+export default function Layout () {
+    return (
+          <div className="min-h-screen flex flex-col">
+               <nav className="flex justify-between items-center bg-blue-700 text-white px-6 py-3 shadow">
+                <div className="flex gap-6 items-center">
+                    <Link className="flex items-center gap-1 hover:underline">
+                         <FaHome/> Home
+                    </Link>
+                    <Link className="flex items-center gap-1 hover:underline">
+                          <FaEnvelope/> Messages
+                    </Link>
+                    <Link className="flex items-center gap-1 hover:underline">
+                        <FaBell/> Notification
+                    </Link>
+                </div>
+                
+                <button className="bg-white text-blue-700 px-3 py-2 rounded-xl font-semibold flex flex-col items-center">
+                    <FaUserCircle className="text-3xl mb-1"/>
+                </button>
+               </nav>
+    </div>
+    )
+}
+
