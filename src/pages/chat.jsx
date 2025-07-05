0b360182fae9428dc6ee904e3ef5ea08b89878cc
nietiene <niyomugaboetiene53@gmail.com>
2025-07-05 11:36:57 -0700

    commit of gikonko-tss-chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -102,6 +102,7 @@ export default function Chat() {
                        onClick={() => {
                         setSelectedUser(user);
                         setMessages([]);
+                        console.log("Selected user:", user);
                        }}
                        className={`flex items-center gap-3 w-full p-2 rounded-lg hover:bg-blue-100 transition
                                    ${user === selectedUser ? "bg-blue-300 font-semibold text-blue-700" : "text-gray-700"}`}
@@ -156,7 +157,7 @@ export default function Chat() {
                </div>
 
                {/* Input area */}
-               {selectedUser && (
+               {selectedUser !== "" && (
                 <div className="p-4 border-t flex gap-2">
                     <input 
                        type="text"
