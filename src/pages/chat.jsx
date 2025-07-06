164c2272084d5e98c5e71abe068f063dd0edc37a
nietiene <niyomugaboetiene53@gmail.com>
2025-07-06 08:57:27 -0700

    commit of gikonko-tss-chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -47,8 +47,12 @@ export default function Chat() {
     useEffect(() => {
         api.get('/api/auth/profile')
         .then((res) => {
+            console.log("Profile info:", res.data);
             setMyUsername(res.data.name);
-            socket.emit('login', res.data.name);
+            socket.emit('login', {
+                name: res.data.name,
+                id: res.data.user_id
+        });
         }).catch(() => {
             navigate('/');
         });
