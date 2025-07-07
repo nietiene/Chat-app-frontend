1b4dfc85668734668895c9b3d7aab852547d8f73
nietiene <niyomugaboetiene53@gmail.com>
2025-07-06 17:22:44 -0700

    commit of gikonko-tss-chat

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -9,6 +9,16 @@ export default function Layout () {
         const [profileImage, setProfileImage] = useState(null);
         const [selectedFile, setSelectedFile] = useState(null);
     
+        const handleFileChange = (e) => {
+            if (e.target.files && e.target.files[0]) {
+                setProfileImage(URL.createObjectURL(e.target.files[0]));
+                setSelectedFile(e.target.files[0]);
+            }
+        }
+
+        const handleProfilePhotoChange = async (e) => {
+            
+        }
     return (
           <div className="min-h-screen flex flex-col">
                <nav className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center bg-blue-700 text-white px-6 py-3 shadow">
@@ -38,7 +48,31 @@ export default function Layout () {
                </nav>
 
                {showUserMenu && (
-                <div className="absolute top-14 right-0 bg-white border rounded"></div>
+                <div className="absolute top-14 right-0 bg-white border rounded shadow p-4 w-64 z-50">
+                    <h3 className="font-bold mb-2">{user.name}</h3>
+                    <p><strong>Phone:</strong>{user.phone}</p>
+                    <p><strong>Role:</strong>{user.role}</p>
+
+                    <form onSubmit={handleProfilePhotoChange} className="mt-3">
+                        <label className="block mb-1 font-semibold">Change Profile photo</label>
+                        <input type="file"
+                           accept="image/*"
+                           onChange={handleFileChange}
+                           className="mb-2"
+                        />
+
+                        <button type="submit"
+                            className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">
+                                Upload
+                            </button>
+                         <button
+                           onClick={handleLogout}
+                           className="mt-4 w-full bg-red-600 text-white px-3 py-2 rounded hover:bg-red-700"
+                        >
+                            Logut
+                        </button>   
+                    </form>
+                </div>
                )}
 
             <div className="pt-20 flex-1 overflow-y-auto">
