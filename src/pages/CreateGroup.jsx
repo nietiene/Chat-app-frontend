4f94b6775495290e0b470c493fb374144bee6f79
nietiene <niyomugaboetiene53@gmail.com>
2025-07-08 13:36:08 -0700

    commit of gikonko-tss-chat

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -60,7 +60,12 @@ export default function CreateGroup () {
 
             <div className="mb-4">
                 <label className="block text-sm font-medium mb-1">Group name</label>
-                <input type="text" name="" id="" />
+                <input type="text"
+                   value={groupName}
+                   onChange={(e) => setGroupName(e.target.value)}
+                   className="w-full border px-3 py-2 rounded focus:ring-2 focus:ring-blue-500"
+                   placeholder="Enter group name"
+                   />
             </div>
         </div>
     )
