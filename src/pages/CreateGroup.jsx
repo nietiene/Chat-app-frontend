81a91c364c82fc7e652e850ae3b15519b6157224
nietiene <niyomugaboetiene53@gmail.com>
2025-07-08 13:30:19 -0700

    commit of gikonko-tss-chat

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../api";
 
 export default function CreateGroup () {
 
     const [groupName, setGroupName] = useSearchParams('');
     const [allUsers, setAllUsers] = useState([]);
+    const [selectedUsers, setSelectedUsers] = useState([]);
+    const [myName, setMyName] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const res = await api.get('/api/auth/profile');
+                setMyName(res.data.name);
+            } catch (error) {
+                navigate('/');
+            }
+        };
+
+        const fetchUsers = async () => {
+            try {
+                const res = await api.get('/api/users');
+                setAllUsers(res.data);
+            } catch (error) {
+                 console.error('Error fetching users:', error);
+            }
+        }
+    })
 } 
