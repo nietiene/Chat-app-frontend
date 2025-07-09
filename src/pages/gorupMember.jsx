30e02d4ccc044aa95644013d08133b1e845c9f0e
nietiene <niyomugaboetiene53@gmail.com>
2025-07-09 08:42:12 -0700

    commit of gikonko-tss-chat

--- src/pages/gorupMember.jsx
+++ src/pages/gorupMember.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../api";
 
 export default function GroupMember () {
     const { g_id } = useParams();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [members, setMembers] = useState([]);
+    const [Loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchMembers = async () => {
+            try {
+                const res = await api.get(`/api/groups/group_members/${g_id}`);
+                
+            }
+        }
+    })
 }
