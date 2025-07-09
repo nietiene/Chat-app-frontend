11bce4dfb77bee043553e1afbe00659a61b1e46e
nietiene <niyomugaboetiene53@gmail.com>
2025-07-09 14:01:50 -0700

    commit of gikonko-tss-chat

--- src/pages/gorupMember.jsx
+++ src/pages/gorupMember.jsx
@@ -72,9 +72,9 @@ export default function GroupMember() {
 
 
   return (
-    <div className="max-w-md mx-auto p-6 bg-white rounded shadow-md mt-6">
-        <div className="flex items-center justify-center mb-6">
-               <button className="flex items-center text-blue-600 hover:text-blue-800 transition-colors" onClick={() => navigate("/chat")}>
+    <div className="max-w-md mx-auto p-6 bg-white rounded shadow-lg mt-6">
+        <div className="flex items-center justify-center mb-8">
+               <button className="flex items-center gap-2 text-blue-600 hover:text-blue-800 transition-colors" onClick={() => navigate("/chat")}>
                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
                 </svg>
