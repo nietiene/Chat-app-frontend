0d0e627d2c2513be3704632f9499cc137cd6155c
nietiene <niyomugaboetiene53@gmail.com>
2025-07-11 10:12:48 -0700

    commit of gikonko-tss-chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -411,7 +411,7 @@ export default function Chat() {
                                     <button
                                       onClick={() => handleDeletePrivateMessage(msg.m_id)}
                                       title='Delete private message'
-                                      className='absolute-top-2 -right-2 text-red-500 opacity-0 group-hover:opacity-100 hover:text-red-700 transition'
+                                      className='absolute -top-2 -right-2 text-red-500 opacity-0 group-hover:opacity-100 hover:text-red-700 transition'
                                     >
                                         <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
