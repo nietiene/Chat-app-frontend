28337c85df759d6199a4e289adcc81de776d0b76
nietiene <niyomugaboetiene53@gmail.com>
2025-07-14 14:11:33 -0700

    commit of gikonko-tss-chat

--- src/pages/ChangeGroupPhoto.jsx
+++ src/pages/ChangeGroupPhoto.jsx
@@ -56,7 +56,10 @@ export default function ChangeGroupPhoto() {
                 </button>
 
                 <button 
-                   className="border px-4 py-2 rounded text-gray-600"></button>
+                   className="border px-4 py-2 rounded text-gray-600 hover:bg-gray-100"
+                >
+                    Cancel
+                </button>
             </div>
           </form>
        </div>
