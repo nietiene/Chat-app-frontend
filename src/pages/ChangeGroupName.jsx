fb52fa0353de20d1dd9c1444655fba117543d90e
nietiene <niyomugaboetiene53@gmail.com>
2025-07-14 09:59:28 -0700

    commit of gikonko-tss-chat

---
+++ src/pages/ChangeGroupName.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+
+export default function changeGroupName() {
+    const navigate = useNavigate();
+}
