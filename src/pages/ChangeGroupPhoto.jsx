c5810530b371b653da6adda6134fd78729b8350c
nietiene <niyomugaboetiene53@gmail.com>
2025-07-14 13:47:57 -0700

    commit of gikonko-tss-chat

---
+++ src/pages/ChangeGroupPhoto.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../api";
+
+export default function ChangeGroupPhoto() {
+    
+}
