5a025f4090795529519ff80d02e857be55e23cc7
nietiene <niyomugaboetiene53@gmail.com>
2025-07-14 13:07:08 -0700

    commit of gikonko-tss-chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -440,14 +440,14 @@ export default function Chat() {
                 
                                         <button
                                             className='w-full px-4 py-2 text-sm text-left text-gray-700 bg-gray-50 rounded-lg hover:text-blue-600 hover:bg-blue-50 transition-colors'
-                                            onClick={navigate(`/changeGroupName/${selectedGroup.g_id}`)}
+                                            onClick={() => navigate(`/changeGroupName/${selectedGroup.g_id}`)}
                                         >
                                           Change group name
                                         </button>
 
                                         <button
                                             className='w-full px-4 py-2 text-sm text-left text-gray-700 bg-gray-50 rounded-lg hover:bg-blue-50 hover:text-blue-600 transition-colors'
-                                            onClick={navigate('/changeGroupPhoto')}
+                                            onClick={() => navigate('/changeGroupPhoto')}
                                         >
                                           Change group photo
                                         </button>
