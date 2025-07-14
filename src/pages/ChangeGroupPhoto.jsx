cf2acb2bddebd7a538617e02c1655567b46b5e07
nietiene <niyomugaboetiene53@gmail.com>
2025-07-14 14:00:27 -0700

    commit of gikonko-tss-chat

--- src/pages/ChangeGroupPhoto.jsx
+++ src/pages/ChangeGroupPhoto.jsx
@@ -8,5 +8,13 @@ export default function ChangeGroupPhoto() {
     const [photo, setPhoto] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const handleChangePhoto
+    const handleChangePhoto = async (e) => {
+        e.preventDefault();
+        if (!photo) return alert('Please select a photo');
+
+        const formData = new FormData();
+        formData.append('photo', photo);
+
+        setLoading(true);
+    }
 }
