889af63db441bf6a154daa0831fa45572fffbccf
nietiene <niyomugaboetiene53@gmail.com>
2025-07-14 09:55:48 -0700

    commit of gikonko-tss-chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -439,24 +439,24 @@ export default function Chat() {
                                     <div className="absolute top-full mt-2 bg-white border rounded shadow-lg p-3 z-20 w-48 space-y-3">
                 
                                         <button
-                                            className='w-full px-4 py-2 text-sm text-left text-gray-700 bg-gray-50 rounded-lg hover:bg-text-50 hover:bg-blue-50 transition-colors'
+                                            className='w-full px-4 py-2 text-sm text-left text-gray-700 bg-gray-50 rounded-lg hover:text-blue-50 hover:bg-blue-50 transition-colors'
                                             onClick={handleDeleteGroup}
                                         >
-                                          ✏️ Change group name
+                                          Change group name
                                         </button>
 
                                         <button
                                             className='w-full px-4 py-2 text-sm text-left text-gray-700 bg-gray-50 rounded-lg hover:bg-blue-50 hover:text-blue-600 transition-colors'
                                             onClick={handleDeleteGroup}
                                         >
-                                         🖼️ Change group photo
+                                          Change group photo
                                         </button>
                             
                                        <button
                                             className='w-full px-4 py-2 text-sm text-left text-red-600 bg-red-50 rounded-lg hover:bg-red-100 hover:text-red-700 transition-colors'
                                             onClick={handleDeleteGroup}
                                         >
-                                           🗑️ Delete Group
+                                            Delete Group
                                         </button>
                                     </div>
                                 )}
