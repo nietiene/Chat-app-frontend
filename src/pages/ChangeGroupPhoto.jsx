c64389543ba814eec8dfc61cc24d3acbcdf3b06e
nietiene <niyomugaboetiene53@gmail.com>
2025-07-14 13:49:05 -0700

    commit of gikonko-tss-chat

--- src/pages/ChangeGroupPhoto.jsx
+++ src/pages/ChangeGroupPhoto.jsx
@@ -3,5 +3,10 @@ import { useParams, useNavigate } from "react-router-dom";
 import api from "../api";
 
 export default function ChangeGroupPhoto() {
-    
+    const { g_id } = useParams();
+    const navigate = useNavigate();
+    const [photo, setPhoto] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleChangePhoto
 }
