76811941c684730e81a2877bf433428e3affed93
nietiene <niyomugaboetiene53@gmail.com>
2025-07-15 08:17:40 -0700

    commit of gikonko-tss-chat

--- src/pages/ChangeGroupPhoto.jsx
+++ src/pages/ChangeGroupPhoto.jsx
@@ -62,7 +62,7 @@ export default function ChangeGroupPhoto() {
             <div className="flex justify-end space-x-3">
                 <button 
                  className="px-4 py-2 text-sm font-medium text-white bg-blue-600 roundend-md  shadow-sm hover:bg-blue-700 focus:outline-none
-                      focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                      focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
                 >
                     {loading ? 'Uploading...' : 'Upload'}
                 </button>
