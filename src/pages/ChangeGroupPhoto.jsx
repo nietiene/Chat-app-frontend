b0b0c47b5e64ff3bb58e04c19593fcb37de5f92c
nietiene <niyomugaboetiene53@gmail.com>
2025-07-15 08:21:38 -0700

    commit of gikonko-tss-chat

--- src/pages/ChangeGroupPhoto.jsx
+++ src/pages/ChangeGroupPhoto.jsx
@@ -70,7 +70,7 @@ export default function ChangeGroupPhoto() {
                 <button 
                    className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                    disabled={loading}
-                   onClick={() => navigate('/chat')}
+                   onClick={() => navigate(-1)} // same as navigate to previous page
                 >
                     Cancel
                 </button>
