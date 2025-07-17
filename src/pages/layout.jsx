e2944cfe61904e71526502062ea5df861b6f1d45
nietiene <niyomugaboetiene53@gmail.com>
2025-07-17 07:35:36 -0700

    commit of gikonko-tss-chat

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -128,7 +128,8 @@ export default function Layout () {
                    <div className="space-y-3">
                         <button type="submit"
                             className=" w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700
-                                        transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-sm font-semibold">
+                                        transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-sm font-semibold"
+                       >
                                 Upload
                             </button>
                          <button
