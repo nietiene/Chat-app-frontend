7c978aeba2101e50ccaf5444ea6a89a010842179
nietiene <niyomugaboetiene53@gmail.com>
2025-07-17 09:58:55 -0700

    commit of gikonko-tss-chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -377,7 +377,7 @@ function formatTimeStamp(timestamp) {
 useEffect(() => {
     if (!myName) return;
 
-    api.get(`/api/messages/last/${encodeURIComponent(myName)}`, {
+    api.get(`/api/messages/last/${myName}`, {
         withCredentials: true
     })
     .then(response => {
