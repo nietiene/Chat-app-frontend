3133b4e6149a7acc0629e42fe29bd962eebb0b58
nietiene <niyomugaboetiene53@gmail.com>
2025-07-19 10:47:48 -0700

    commit of gikonko-tss-chat

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -71,7 +71,7 @@ export default function Dashboard() {
         day: 'numeric',
         hour: 'numeric',
         minute: '2-digit',
-        hour12: true
+        hour12: false
       })
 } 
 
@@ -180,11 +180,9 @@ export default function Dashboard() {
            </div>
 
 
-           <div className="sticky top-12 z-10 mb-4 bg-white py-2">
               <h2 className="text-xl font-semibold text-gray-700 border-b border-gray-200 pb-2 px-1">
                 Recent Posts
              </h2>
-           </div>
 
               {post.map((post) => {
               return (
