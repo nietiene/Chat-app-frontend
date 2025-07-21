ad0eebb7be6959d5e34bc6489536547149ef3b4d
nietiene <niyomugaboetiene53@gmail.com>
2025-07-21 08:02:48 -0700

    commit of gikonko-tss-chat

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -52,10 +52,11 @@ export default function Login() {
                       value={password} required
                       onChange={(e) => setPassword(e.target.value)}
                       className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-/> <br />
+               /> <br />
                 </div>
 
-                <button type="submit">Login</button>
+                <button type="submit"
+                   className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300">Login</button>
             </form>
 </div>
 
