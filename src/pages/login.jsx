d8b1675cafe80dd2bac8bb52d38d61f6c2fb7eeb
nietiene <niyomugaboetiene53@gmail.com>
2025-07-21 07:58:27 -0700

    commit of gikonko-tss-chat

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -40,7 +40,7 @@ export default function Login() {
                       value={phone} 
                       required
                       onChange={(e) => setPhone(e.target.value)}
-                      className="w-full px-4 py-2 border border-gray-300 rounded-lg "
+                      className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
                     /> <br />
                 </div>
 
