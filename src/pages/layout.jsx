d3925246db67af85882b5fc326b2921243c97fa2
nietiene <niyomugaboetiene53@gmail.com>
2025-07-23 07:49:48 -0700

    commit of gikonko-tss-chat

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -70,6 +70,8 @@ export default function Layout () {
                     const unreadRes = await api.get(`/api/messages/unread`);
 
                     // here loops through each message and sums up their unread_count values starting from 0
+                    // is methood that loops through the array and sums up the values
+                    // it start from zero, msg is each item in the array(each message)
                     const totalUnreadMessages = unreadRes.data.reduce((sum, msg) => sum + msg.unread_count, 0);
                     setUnreadMessages(totalUnreadMessages);
                     
